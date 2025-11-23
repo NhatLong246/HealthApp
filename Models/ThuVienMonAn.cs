5472a90e3a8d5a859487a37578259b7b73023446
Nguyen Loc <nloc22102@gmail.com>
2025-11-23 15:47:48 +0700

    Refactor BuaAnChiTiet and ThuVienMonAn models: update table mappings, remove unused properties, and adjust data annotations for better alignment with database schema.

--- Models/ThuVienMonAn.cs
+++ Models/ThuVienMonAn.cs
@@ -5,36 +5,38 @@ namespace HealthApp.Models
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
 
-    [Table("DinhDuongMonAn")]
+    [Table("ThuVienMonAn")]
     public partial class ThuVienMonAn
     {
         [Key]
         [StringLength(20)]
-        [Column("MonAnID")]
         public string MonAnID { get; set; }
 
-        [StringLength(200)]
-        [Column("HinhAnh")]
+        [StringLength(500)]
         public string imageURL { get; set; }
 
+        [Required]
         [StringLength(200)]
-        [Column("TenMonAn")]
         public string TenMonAn { get; set; }
 
-        [StringLength(20)]
-        [Column("DonViTinh")]
+        [StringLength(100)]
+        public string Loai { get; set; }
+
+        [StringLength(10)]
         public string Donvi { get; set; }
 
-        [Column("LuongCalo")]
+        public double? KhoiLuongChuan { get; set; }
+
         public double? Calories { get; set; }
 
-        [Column("Protein")]
         public double? Protein { get; set; }
 
-        [Column("ChatBeo")]
+        public double? Carbs { get; set; }
+
         public double? Fat { get; set; }
 
-        [Column("Carbohydrate")]
-        public double? Carbs { get; set; }
+        public double? Fiber { get; set; }
+
+        public DateTime? NgayTao { get; set; }
     }
 }
